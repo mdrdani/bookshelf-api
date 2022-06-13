fc53f869f47ca62453d50220ade46bb8f915b84e
mdrdani <sudokill3004@gmail.com>
2022-06-13 06:42:28 +0000

    Delete book

--- src/handler.js
+++ src/handler.js
@@ -14,9 +14,7 @@ const createBook = (request, h) => {
   } = request.payload;
 
   const id = nanoid(16);
-  const selesai = true;
-  const belum = false;
-  const finished = pageCount === readPage ? selesai : belum;
+  const finished = pageCount === readPage;
   const insertedAt = new Date().toISOString();
   const updatedAt = insertedAt;
 
@@ -147,6 +145,25 @@ const updateBook = (request, h) => {
 
 const deleteBook = (request, h) => {
   const { bookId } = request.params;
+
+  const index = books.findIndex((book) => book.id === bookId);
+
+  if (index !== -1) {
+    books.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Buku berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Buku gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
 };
 
 module.exports = {
